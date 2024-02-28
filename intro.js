57ca53a671a53a3a4d893a23197fbb6fd93d08cb
cerendn <cerendncer@outlook.com>
2024-02-28 22:54:08 +0300

    update

--- intro.js
+++ intro.js
@@ -1,18 +1,18 @@
 //String " "
 const myString = "Hello World";
-console.log(typeof(myString));
+console.log(typeof (myString));
 
 //Number
-const PI = 3.14 ;
-console.log(typeof(PI));
+const PI = 3.14;
+console.log(typeof (PI));
 
 //Boolean
-const sunny = true ;
-console.log(typeof(sunny));
+const sunny = true;
+console.log(typeof (sunny));
 
 //Undefined
 let color;
-console.log(typeof(color));
+console.log(typeof (color));
 
 const sentence = "O ilk kayıtsızlığım belki de sezgisel bir savunma biçimiydi; ciddi tavrımı takınma, aradaki mesafeyi koruma da olabilirdi ama işe yaradı.İnsanın kendine,kendi dışında bir özün gözle­riyle bakabilmesi her zaman işe yarar. Bu bir alıştırmadır ve öğ­renilebilir."
 console.log(sentence.length);
@@ -20,7 +20,7 @@ console.log(sentence.toLowerCase());//tüm harfleri küçük harf
 console.log(sentence.includes('mesafe'));//içinde kelime arama
 console.log(sentence.split());//string to array
 console.log(sentence.split(' '));//bosluklardan array'e bölme
-console.log(sentence.replace('takınma','takılma'));//değiştirme
+console.log(sentence.replace('takınma', 'takılma'));//değiştirme
 console.log(sentence.charAt(40));//indexdeki karakter
 console.log(sentence.lastIndexOf('a'));// gectiği son index
 console.log(sentence.indexOf('l'));//gectiği ilk index
@@ -28,11 +28,22 @@ console.log(sentence.trim());//bastaki ve sondaki boslukları kesme
 
 const studentName = "Lily Harper";
 const course = "Pilates Eğitmenliği";
-const startday ="20-02-2024";
+const startday = "20-02-2024";
 console.log(
     `Değerli kursiyerimiz ${studentName}, ${course} eğitiminiz ${startday} tarihinde başlayacaktır.`
 );
 
-
-
+const title = "Düz Yürüyen ' Yengeç' ";
+let i = 0;
+while (i < title.length) {
+    if (title[i] === " " || title[i === "' '"]) { i++ }
+    else {
+        console.log(title[i]);
+        i++;
+    }
+}
+//tersten yazdırma 
+for(let i = title.length -1 ; i >= 0 ; i--) {
+    console.log(title[i]);
+     }
 
